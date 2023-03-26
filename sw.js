b7346a456ee6cc76dc496d2ea887fb278b877951
LuisGonzalez558 <a1120250002@utch.edu.mx>
2023-03-25 18:24:28 -0600

    primercommit

---
+++ sw.js
@@ -0,0 +1,62 @@
+//Asignar nombre y version de la cache
+const CACHE_NAME = 'v1_cache_BCH_PWA';
+
+//configuracion de los ficheros
+var urlsToCache = [
+    '/index.html',
+    '/index.css',
+    '/sw.js',
+    '/main.js',
+    '/manifest.js',
+    '/img/windows11/Square44x44Logo.altform-unplated_targetsize-256.png',
+    '/img/windows11/llamada-telefonica.png',
+    '/img/windows11/linkedin.png',
+    '/img/windows11/email.png',
+    '/img/windows11/developer-3461405_1280.png',
+    '/img/windows11/Trabajando.jpg',
+    '/img/windows11/Estudios.jpg'
+];
+
+self.addEventListener('install', event => { 
+    event.waitUntil(caches.open(CACHE_NAME)
+    .then(cache => { 
+        return cache
+        .addAll(urlsToCache)
+        .then(() => { 
+            self.skipWaiting() 
+        })
+    .catch(err => console.log('Hubo un error', err)) })); });
+
+self.addEventListener('activate', e => {
+    const cacheWhitelist = [CACHE_NAME];
+
+
+    e.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(cacheNames => {
+                        if (cacheWhitelist.indexOf(cacheNames) == -1) {
+                            return cache.delete(cacheNames);
+                        }
+                    })
+                );
+            })
+            .then(() => {
+                self.clients.claim();
+            })
+    );
+
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request)
+            .then(res => {
+                if (res) {
+                    return res;
+                }
+                return fetch(e.request);
+            })
+    );
+});
