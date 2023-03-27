3cffa0af26c7420f4f02dcf0e81f4d719406fd5c
LuisGonzalez558 <a1120250002@utch.edu.mx>
2023-03-26 18:38:06 -0600

    14commit

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ var urlsToCache = [
 
 self.addEventListener('install', event => { 
     event.waitUntil(caches.open(CACHE_NAME)
-    .then(function (cache) { 
+    .then((cache) => { 
         return cache
         .addAll(urlsToCache)
         .then(() => { 
